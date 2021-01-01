88f2dfd12df91f2e592728471315ca314a6816d7
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-01 16:43:47 -0300

    Aula10 - EF Core - Parte 4 - Criação de mapeamento para as entidades. - Normalização e/ou definição correta das propriedades das tabelas.

--- Data/StoreDataContext.cs
+++ Data/StoreDataContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data.Maps;
 using ProductCatalog.Models;
 
 namespace ProductCatalog.Data
@@ -8,8 +9,15 @@ namespace ProductCatalog.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
             optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=balta_1976;Username=postgres;Password=postgres;Port=5433");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductMap());
+            builder.ApplyConfiguration(new CategoryMap());
         }
 
     }
