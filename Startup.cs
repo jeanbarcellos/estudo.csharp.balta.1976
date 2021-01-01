de81edb6b39b64d2e2d250848f837e08b32fef2f
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-01 19:50:00 -0300

    Aula 12 – Adição do MVC

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,8 @@ namespace ProductCatalog
         // Este método é chamado pelo tempo de execução. Use este método para adicionar serviços ao contêiner.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adicionar o serviço MVC a coleção de serviços
+            services.AddMvc();
         }
 
         // Este método é chamado pelo tempo de execução. Use este método para configurar o pipeline de solicitação HTTP.
@@ -18,8 +20,12 @@ namespace ProductCatalog
         {
             if (env.IsDevelopment())
             {
+                // Captura instâncias System.Exception síncronas e assíncronas do pipeline e gera respostas de erro HTML.
                 app.UseDeveloperExceptionPage();
             }
+
+            // Adiciona MVC ao pipeline de execução do request
+            app.UseMvc();
         }
     }
 }
