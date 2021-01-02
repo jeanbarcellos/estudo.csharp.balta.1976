930a2a17b2be249f71293f8997ca9575e66c271b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-02 00:36:11 -0300

    Remoção de importaçoes desnecerrárias

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,10 +1,6 @@
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using ProductCatalog.Data;
 using ProductCatalog.Models;
-using System;
 using ProductCatalog.Repositories;
 
 namespace ProductCatalog.Controllers
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using ProductCatalog.Data;
 using ProductCatalog.Models;
 using ProductCatalog.Repositories;
 using ProductCatalog.ViewModels.ProductViewModels;
