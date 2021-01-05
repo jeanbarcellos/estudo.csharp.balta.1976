3074aa7c2dfd88b211b28331428264f52524f648
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-05 17:03:42 -0300

    Startup.cs - Estudo da classe. Adição de comentários para melhor entendimento!

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using ProductCatalog.Data;
 using ProductCatalog.Repositories;
+using System;
 
 
 namespace ProductCatalog
@@ -12,46 +14,62 @@ namespace ProductCatalog
     public class Startup
     {
 
-        // Este método é chamado pelo tempo de execução. Use este método para adicionar serviços ao contêiner.
+        // Método para adicionar serviços ao contêiner.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Adiciona o DataContext como dependência
-            services.AddScoped<StoreDataContext, StoreDataContext>();
-
+            // Adiciona serviços para controladores ao IServiceCollection.
+            // Este método não registrará serviços usados para visualizações ou páginas.
             services.AddControllers();
 
             // Adição do serviço de compressão de response.
             services.AddResponseCompression();
 
+            // Adiciona o DataContext como dependência
+            services.AddScoped<StoreDataContext, StoreDataContext>();
+
+            // Adiciona um serviço temporário do tipo especificado em TService
             services.AddTransient<CategoryRepository, CategoryRepository>();
             services.AddTransient<ProductRepository, ProductRepository>();
 
+            // Registrar o gerador Swagger, definindo mais documentos Swagger
             services.AddSwaggerGen(x =>
             {
                 x.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
         }
 
-        // Este método é chamado pelo tempo de execução. Use este método para configurar o pipeline de solicitação HTTP.
+        // Método para configurar o pipeline de solicitação HTTP.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Verifica qual ambiente o App se encontra
             if (env.IsDevelopment())
             {
-                // Captura instâncias System.Exception síncronas e assíncronas do pipeline e gera respostas de erro HTML.
+                // Middleware para capturar instâncias System.Exception síncronas e assíncronas do pipeline e gera respostas de erro HTML.
                 app.UseDeveloperExceptionPage();
             }
 
+            // Middleware de roteamento para rotear as requests (resolução de endpoint)
             app.UseRouting();
 
+            // Middleware de despacho de endpoint (Dispatcher - Executar o endpoint correspondente)
             app.UseEndpoints(endpoints =>
             {
+                // Configuração do mapa de rota
                 endpoints.MapControllers();
+
+                // Configuração manual
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
 
+            // Middleware para compactar dinamicamente as respostas HTTP.
             app.UseResponseCompression();
 
-
+            // Middleware para servir o Swagger gerado como um terminal JSON.
             app.UseSwagger();
+            // Middleware para servir swagger-ui (HTML, JS, CSS, etc.), especificando o endpoint JSON Swagger
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jean Barcellos - V1");
